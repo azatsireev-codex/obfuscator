5acc308162a6e7931095b94dde92f650238ee08f
Akat <azat.sireev@gmail.com>
2026-02-27 19:38:10 +0300

    Add files via upload

---
+++ orebfuscator-api/src/main/java/net/imprex/orebfuscator/api/OrebfuscatorService.java
@@ -0,0 +1,28 @@
+package net.imprex.orebfuscator.api;
+
+import java.util.Collection;
+
+import org.bukkit.block.Block;
+
+/**
+ * <p>
+ * Service that gives access to some of Orebfuscators internal methods. Use with caution.
+ * </p>
+ *
+ * <p>
+ * All calls to this service are expected to originate from the servers main-thread or in case of folia from a region
+ * thread (never the global region thread).
+ * </p>
+ *
+ * @since 5.2.0
+ */
+public interface OrebfuscatorService {
+
+  /**
+   * Deobfuscates a list of blocks. All blocks are expected to be located in the same world. It is recommended to call
+   * this method after the block update.
+   *
+   * @param blocks list of blocks to deobfuscate
+   */
+  void deobfuscate(Collection<? extends Block> blocks);
+}
